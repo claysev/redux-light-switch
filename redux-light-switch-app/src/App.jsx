76d5db97e98660b5a5688e4ba5d6441d54c41146
Clayton Severo <clayton.severo@rankme.com.br>
2024-05-13 16:32:36 -0300

    Separando componente

--- redux-light-switch-app/src/App.jsx
+++ redux-light-switch-app/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Toggle from './components/Toggle'
 import { toggle } from "./state/store";
 
 function App({ state, dispatch }) {
@@ -6,28 +7,14 @@ function App({ state, dispatch }) {
       dispatch(toggle())
    };
 
-   const lightIs = state === "off" ? "bg-sky-950" : "bg-amber-400";
-   const bgLabel = state === "off" ? "bg-sky-800" : "bg-slate-50"
+   const bgContainer = state === "off" ? "bg-sky-950" : "bg-amber-400";
+   const bgLabel = state === "off" ? "bg-sky-800" : "bg-slate-50";
+   const lightIs = state === "off" ? "on" : "off" ;
 
    return (
-      <div className={`flex justify-center items-center h-screen transition delay-50 duration-200 ease-in-out ${lightIs}`}>
-         <label className={`swap swap-flip min-w-44 rounded-full p-3 ${bgLabel} opacity-75 shadow-lg`}>
-            <input type="checkbox" onChange={handleToggle} />
-            <svg
-               className="swap-on fill-amber-800 w-10 h-10"
-               xmlns="http://www.w3.org/2000/svg"
-               viewBox="0 0 24 24"
-            >
-               <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
-            </svg>
-            <svg
-               className="swap-off fill-slate-300 w-10 h-10 "
-               xmlns="http://www.w3.org/2000/svg"
-               viewBox="0 0 24 24"
-            >
-               <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-            </svg>
-         </label>
+      <div className={`flex flex-col justify-center items-center gap-4 h-screen transition delay-50 duration-200 ease-in-out ${bgContainer}`}>
+         <h1 className={`text-4xl font-bold ${lightIs === "on" ? "text-slate-300" : "text-amber-700"}`}>Turn the light {lightIs}!</h1>
+         <Toggle handleToggle={handleToggle} backgroundLabel={bgLabel} />
       </div>
    );
 }
